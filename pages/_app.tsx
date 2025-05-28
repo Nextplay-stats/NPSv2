a07c72fc5ef42b1fba003a1c98a8d04a00d7cde4
Nextplay-stats <nextplaystats@outlook.com>
2025-05-28 15:43:16 +0100

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+
+const msalConfig = {
+  auth: {
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID || '',
+    authority: `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_TENANT_ID}`,
+    redirectUri: '/',
+  },
+};
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MsalProvider instance={msalInstance}>
+      <Component {...pageProps} />
+    </MsalProvider>
+  );
+}
