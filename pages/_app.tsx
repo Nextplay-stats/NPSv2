49b1181b6401c5a30fb97ef6cc60c04bf502e3d7
Nextplay-stats <nextplaystats@outlook.com>
2025-05-28 21:04:58 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
+// _app.tsx
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { PublicClientApplication } from '@azure/msal-browser';
 import { MsalProvider } from '@azure/msal-react';
 
-const msalConfig = {
+const msalInstance = new PublicClientApplication({
   auth: {
-    clientId: process.env.NEXT_PUBLIC_CLIENT_ID || '',
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID!,
     authority: `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_TENANT_ID}`,
-    redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI || 'https://zealous-sky-0785c6503.6.azurestaticapps.net/dashboard',
+    redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI,
   },
-};
-
-const msalInstance = new PublicClientApplication(msalConfig);
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
